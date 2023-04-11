efbbe67f565200f521902b7c33d8694e59a6c61f
Marina <78799773+marinezh@users.noreply.github.com>
2023-04-12 01:41:04 +0300

    add video to the home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import videoBG from "../assets/video.mp4";
 
 const Home = () => {
   return (
     <div>
-      <div className="home" style={{ margin: "2rem 1rem" }}>
+      <div>
+        <video src={videoBG} autoPlay loop muted></video>
+      </div>
+      <div className="home_content" style={{ margin: "2rem 1rem" }}>
         <span>Countries app </span>is a simple React application made in
         Business College Helsinki lessons. App uses{" "}
         <a href="https://restcountries.com/">https://restcountries.com/ </a> and{" "}
